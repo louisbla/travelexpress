e69870628783adb2b822a76e8b2cfd5f7788045a
Fadoua Hamouachy <fadoua.hamouachy@live.fr>
2019-06-02 18:27:50 -0400

    Proposer un trajet

---
+++ Controllers/TrajetController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Garderie.Models;
+using Garderie.Data;
+using Garderie.ViewModels.ArticleViewModels;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Garderie.Controllers
+{
+    public class TrajetController : Controller
+    {
+        private readonly GarderieContext _context;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public TrajetController(GarderieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,conducteur,ville_depart,ville_arrivee,date_heure,nb_passagers")] Trajet trajet)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(trajet);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(trajet);
+        }
+    }
+}
---
+++ Models/Trajet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Garderie.Models
+{
+    public class Trajet
+    {
+        public int id { get; set; }
+        public int conducteur { get; set; }
+        [Display(Name = "Ville de départ")]
+        public String ville_depart { get; set; }
+        [Display(Name = "Ville d'arrivée")]
+        public String ville_arrivee { get; set; }
+        public DateTime date_heure { get; set; }
+        public int nb_passagers { get; set; }
+
+    }
+}
